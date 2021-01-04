53ca347f9abc58462d7148814b864c6a441d22f9
Josef Gruber <seppix@freenet.de>
2021-01-04 22:43:28 +0100

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"eta-data adapter settings": {
+		"en": "Adapter settings for eta-data",
+		"de": "Adaptereinstellungen für eta-data",
+		"ru": "Настройки адаптера для eta-data",
+		"pt": "Configurações do adaptador para eta-data",
+		"nl": "Adapterinstellingen voor eta-data",
+		"fr": "Paramètres d'adaptateur pour eta-data",
+		"it": "Impostazioni dell'adattatore per eta-data",
+		"es": "Ajustes del adaptador para eta-data",
+		"pl": "Ustawienia adaptera dla eta-data",
+		"zh-cn": "eta-data的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
